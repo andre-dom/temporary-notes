2359bfba6152c97beb1168d06517dad248f5e20e
andre <andre.domingo@sjsu.edu>
2021-06-01 16:09:02 -0700

    added github link

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Link } from 'react-router-dom'
 import { Container } from  'react-bootstrap'
 import Post from './Post/Post'
 import NewPost from './Post/NewPost'
+import GithubImage from 'images/github.svg'
 
 const App = () => {
     return (
     <Container className="d-flex flex-column align-items-center">
+        <a style={{
+            position: 'absolute',
+            right: 5,
+            top: 5,
+        }} href="https://github.com/andre-dom/temporary-notes" target="_blank" >
+              <img src={GithubImage} alt="Github" width="50" height="50"/>
+          </a>
+
+
         <Switch>
             <Route exact path='/' component={NewPost}/>
             <Route exact path='/posts/:uuid' component={Post}/>
--- app/javascript/components/Post/PostCard.jsx
+++ app/javascript/components/Post/PostCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Redirect, Link } from 'react-router-dom'
-import { Card } from  'react-bootstrap'
-import Countdown from 'react-countdown';
+import { Card, Button } from  'react-bootstrap'
+import Countdown from 'react-countdown'
 
 const PostCard = ( { post } ) => {
     console.log(post)
